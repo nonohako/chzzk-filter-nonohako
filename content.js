a2de3dc7d9cc0d048af497e348687bb575c61be0
demd7362 <demd7362@gmail.com>
2025-02-10 23:16:53 +0900

    refactor: live stream 요청 딜레이때문에 setTimeout 추가

--- content.js
+++ content.js
@@ -1,38 +1,33 @@
 console.log("chzzk filter activated")
 
-chrome.storage.sync.get(['streamerNames'], function (result) {
-  console.log('filtered streamer : ', result.streamerNames)
-})
-
-// 필터링 함수
 function filterStreamers() {
   chrome.storage.sync.get(['streamerNames'], function (result) {
     const streamerNames = new Set(result.streamerNames || [])
     if (streamerNames.size > 0) {
-      const listItems = document.querySelectorAll('li')
-      listItems.forEach(function (item) {
-        const streamerName = item.querySelector('.video_card_name__Y3icO .name_text__yQG50')?.textContent
+      document.querySelectorAll('li').forEach(function (node) {
+        const streamerName = node.querySelector('.name_text__yQG50')?.textContent
         if (streamerNames.has(streamerName)) {
-          item.remove()
+          node.remove()
         }
       })
     }
+
   })
 }
 
-// 초기 로드 시 필터링 적용
-filterStreamers()
 
-// 동적 콘텐츠 감시를 위한 MutationObserver
 const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    mutation.addedNodes.forEach((node) => {
-      if (node.nodeType === 1 && node.matches('li')) {
-        filterStreamers() // 새로운 <li>가 추가될 때마다 필터링 적용
-      }
+  setTimeout(() => {
+    mutations.forEach((mutation) => {
+      mutation.addedNodes.forEach((node) => {
+        if (node.nodeType === 1 && node.matches('li')) {
+          filterStreamers() // 새로운 <li>가 추가될 때마다 필터링 적용
+        }
+      })
     })
-  })
+  }, 70)
 })
 
-// body 요소 감시 시작
-observer.observe(document.body, { childList: true, subtree: true })
+// body 요소 감시
+observer.observe(document.body, {childList: true, subtree: true})
+
