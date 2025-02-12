94d20806c0bae0e2a2ba13dc6f23218d3955f69c
demd7362 <demd7362@gmail.com>
2025-02-12 12:28:47 +0900

    refactor: remove 처리 후 영상 로딩되게 수정

--- content.js
+++ content.js
@@ -1,5 +1,8 @@
 console.log("chzzk filter activated")
 
+let removes = []
+let removeSet = new Set()
+
 function filterStreamers() {
   chrome.storage.sync.get(['streamerNames', 'tags'], function (result) {
     const streamerNames = new Set(result.streamerNames || [])
@@ -8,18 +11,25 @@ function filterStreamers() {
     const streamerName = node.querySelector('.name_text__yQG50')?.textContent
     if (streamerNames.has(streamerName)) {
       node.style.visibility = 'hidden'
+      if(!removeSet.has(node)){
+        removeSet.add(node)
+        removes.push(node)
+      }
     } else {
       const tagNodes = node.querySelectorAll('.video_card_category__xQ15T')
       if (tagNodes.length > 0) {
         const ownTags = Array.from(tagNodes).map(node => node.textContent)
         if (ownTags.some(tag => tags.has(tag))) {
           node.style.visibility = 'hidden'
+          if(!removeSet.has(node)){
+            removeSet.add(node)
+            removes.push(node)
+          }
         }
       }
     }
 
     })
-
   })
 }
 
@@ -29,6 +39,9 @@ const observer = new MutationObserver((mutations) => {
     mutation.addedNodes.forEach((node) => {
       if (node.matches('li')) {
         filterStreamers() // 새로운 <li>가 추가될 때마다 필터링 적용
+        removeSet.forEach(node => node.remove())
+        removes = []
+        removeSet = new Set()
       }
     })
   })
