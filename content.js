81909af69724f033800403a5b082f1d5b3fbba37
demd7362 <demd7362@gmail.com>
2025-02-12 10:58:02 +0900

    refactor: remove -> visibility 조절로 변경, 그에 따라 단일 노드만 조절하게 변경 및 setTimeout 삭제

--- content.js
+++ content.js
@@ -1,24 +1,24 @@
 console.log("chzzk filter activated")
 
-function filterStreamers() {
+function filterStreamers(node) {
   chrome.storage.sync.get(['streamerNames', 'tags'], function (result) {
     const streamerNames = new Set(result.streamerNames || [])
     const tags = new Set(result.tags || [])
-    document.querySelectorAll('li').forEach(function (node) {
-      const streamerName = node.querySelector('.name_text__yQG50')?.textContent
-      if (streamerNames.has(streamerName)) {
-        node.remove()
-      } else {
-        const tagNodes = node.querySelectorAll('.video_card_category__xQ15T')
-        if(tagNodes.length > 0){
-          const ownTags = Array.from(tagNodes).map(node => node.textContent)
-          if(ownTags.some(tag => tags.has(tag))){
-            node.remove()
-          }
+    // document.querySelectorAll('li').forEach(function (node) {
+    const streamerName = node.querySelector('.name_text__yQG50')?.textContent
+    if (streamerNames.has(streamerName)) {
+      node.style.visibility = 'hidden'
+    } else {
+      const tagNodes = node.querySelectorAll('.video_card_category__xQ15T')
+      if (tagNodes.length > 0) {
+        const ownTags = Array.from(tagNodes).map(node => node.textContent)
+        if (ownTags.some(tag => tags.has(tag))) {
+          node.style.visibility = 'hidden'
         }
       }
+    }
 
-    })
+    // })
 
   })
 }
@@ -26,15 +26,13 @@ function filterStreamers() {
 
 const observer = new MutationObserver((mutations) => {
   chrome.storage.sync.get(['delay'], function (result) {
-    setTimeout(() => {
-      mutations.forEach((mutation) => {
-        mutation.addedNodes.forEach((node) => {
-          if (node.matches('li')) {
-            filterStreamers() // 새로운 <li>가 추가될 때마다 필터링 적용
-          }
-        })
+    mutations.forEach((mutation) => {
+      mutation.addedNodes.forEach((node) => {
+        if (node.matches('li')) {
+          filterStreamers(node) // 새로운 <li>가 추가될 때마다 필터링 적용
+        }
       })
-    }, result.delay || 100)
+    })
   })
 })
 
