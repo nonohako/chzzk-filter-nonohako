f073afb57affbc138550a613c1886722895938e0
demd7362 <demd7362@gmail.com>
2025-02-15 17:37:12 +0900

    refactor: 컨텍스트 메뉴 뜨는 로직 수정

--- content.js
+++ content.js
@@ -37,22 +37,23 @@ function filterStreamers() {
     })
   })
 }
-function handleFilterItem(value, type){
-  chrome.storage.sync.get(['streamerNames', 'tags'], function(result) {
+
+function handleFilterItem(value, type) {
+  chrome.storage.sync.get(['streamerNames', 'tags'], function (result) {
     const streamerNames = result.streamerNames ?? []
     const tags = result.tags ?? []
-    switch(type) {
+    switch (type) {
       case 'streamerName': {
         if (!streamerNames.includes(value)) {
           streamerNames.push(value)
-          chrome.storage.sync.set({ 'streamerNames': streamerNames }, filterStreamers)
+          chrome.storage.sync.set({'streamerNames': streamerNames}, filterStreamers)
         }
         break
       }
       case 'tag': {
         if (!tags.includes(value)) {
           tags.push(value)
-          chrome.storage.sync.set({ 'tags': tags }, filterStreamers)
+          chrome.storage.sync.set({'tags': tags}, filterStreamers)
         }
         break
       }
@@ -62,79 +63,87 @@ function handleFilterItem(value, type){
 }
 
 function handleCustomMenu(e) {
-  e.preventDefault()
-  chrome.storage.sync.get(['streamerNames', 'tags'], (result) => {
-    const customMenu = document.getElementById('custom-context-menu')
-    customMenu.style.display = 'block'
-    customMenu.style.left = `${e.pageX}px`
-    customMenu.style.top = `${e.pageY}px`
-    customMenu.innerHTML = ''
+  // 스트리머명 먼저 조회
+  const streamerName = this.querySelector('.name_text__yQG50')?.textContent
+  if(streamerName){
+    e.preventDefault()
+  } else {
+    return
+  }
+  const customMenu = document.getElementById('custom-context-menu')
+  customMenu.style.display = 'block'
+  customMenu.style.left = `${e.pageX}px`
+  customMenu.style.top = `${e.pageY}px`
+  customMenu.innerHTML = ''
 
-    // 스트리머명 차단
-    const streamerName = this.querySelector('.name_text__yQG50')?.textContent
-    const streamerBlockDiv = document.createElement('div')
-    streamerBlockDiv.textContent = `${streamerName} 차단`
-    streamerBlockDiv.style.color = 'green'
-    streamerBlockDiv.style.cursor = 'pointer'
-    streamerBlockDiv.style.fontFamily = 'Arial, sans-serif'
-    streamerBlockDiv.style.fontSize = '14px'
-    streamerBlockDiv.style.padding = '5px'; // 패딩 추가 (호버 시 더 보기 좋게)
-    streamerBlockDiv.addEventListener('click', () => handleFilterItem(streamerName, 'streamerName'))
+  const streamerBlockDiv = document.createElement('div')
+  streamerBlockDiv.textContent = `${streamerName} 차단`
+  streamerBlockDiv.style.color = 'green'
+  streamerBlockDiv.style.cursor = 'pointer'
+  streamerBlockDiv.style.fontFamily = 'Arial, sans-serif'
+  streamerBlockDiv.style.fontSize = '14px'
+  streamerBlockDiv.style.padding = '5px'; // 패딩 추가 (호버 시 더 보기 좋게)
+  streamerBlockDiv.addEventListener('click', () => handleFilterItem(streamerName, 'streamerName'))
 
-    // 호버 시 스타일
-    streamerBlockDiv.addEventListener('mouseover', () => {
-      streamerBlockDiv.style.backgroundColor = '#f0f0f0'
-      streamerBlockDiv.style.color = '#ff0000'
-    })
-    streamerBlockDiv.addEventListener('mouseout', () => {
-      streamerBlockDiv.style.backgroundColor = '' // 원래 배경색으로 복원
-      streamerBlockDiv.style.color = 'green' // 원래 글자색으로 복원
-    })
-
-    customMenu.appendChild(streamerBlockDiv)
-
-    // 태그 차단
-    const tagNodes = this.querySelectorAll('.video_card_category__xQ15T')
-    if (tagNodes.length > 0) {
-      const ownTags = Array.from(tagNodes).map(node => node.textContent)
-      for (const tag of ownTags) {
-        const tagBlockDiv = document.createElement('div')
-        tagBlockDiv.textContent = `${tag} 태그 차단`
-        tagBlockDiv.style.color = 'green'
-        tagBlockDiv.style.cursor = 'pointer'
-        tagBlockDiv.style.fontFamily = 'Arial, sans-serif'
-        tagBlockDiv.style.fontSize = '14px'
-        tagBlockDiv.style.padding = '5px'; // 패딩 추가 (호버 시 더 보기 좋게)
-        tagBlockDiv.addEventListener('click', () => handleFilterItem(tag, 'tag'))
-
-        // 호버 시 스타일
-        tagBlockDiv.addEventListener('mouseover', () => {
-          tagBlockDiv.style.backgroundColor = '#f0f0f0'
-          tagBlockDiv.style.color = '#ff0000'
-        })
-        tagBlockDiv.addEventListener('mouseout', () => {
-          tagBlockDiv.style.backgroundColor = '' // 원래 배경색으로 복원
-          tagBlockDiv.style.color = 'green' // 원래 글자색으로 복원
-        })
-
-        customMenu.appendChild(tagBlockDiv)
-      }
-    }
+  // 호버 시 스타일
+  streamerBlockDiv.addEventListener('mouseover', () => {
+    streamerBlockDiv.style.backgroundColor = '#f0f0f0'
+    streamerBlockDiv.style.color = '#ff0000'
   })
+  streamerBlockDiv.addEventListener('mouseout', () => {
+    streamerBlockDiv.style.backgroundColor = '' // 원래 배경색으로 복원
+    streamerBlockDiv.style.color = 'green' // 원래 글자색으로 복원
+  })
+
+  customMenu.appendChild(streamerBlockDiv)
+
+  // 태그 차단
+  const tagNodes = this.querySelectorAll('.video_card_category__xQ15T')
+  if (tagNodes.length > 0) {
+    const ownTags = Array.from(tagNodes).map(node => node.textContent)
+    for (const tag of ownTags) {
+      const tagBlockDiv = document.createElement('div')
+      tagBlockDiv.textContent = `${tag} 태그 차단`
+      tagBlockDiv.style.color = 'green'
+      tagBlockDiv.style.cursor = 'pointer'
+      tagBlockDiv.style.fontFamily = 'Arial, sans-serif'
+      tagBlockDiv.style.fontSize = '14px'
+      tagBlockDiv.style.padding = '5px'; // 패딩 추가 (호버 시 더 보기 좋게)
+      tagBlockDiv.addEventListener('click', () => handleFilterItem(tag, 'tag'))
+
+      // 호버 시 스타일
+      tagBlockDiv.addEventListener('mouseover', () => {
+        tagBlockDiv.style.backgroundColor = '#f0f0f0'
+        tagBlockDiv.style.color = '#ff0000'
+      })
+      tagBlockDiv.addEventListener('mouseout', () => {
+        tagBlockDiv.style.backgroundColor = '' // 원래 배경색으로 복원
+        tagBlockDiv.style.color = 'green' // 원래 글자색으로 복원
+      })
+
+      customMenu.appendChild(tagBlockDiv)
+    }
+  }
 }
-document.addEventListener('click', function (event) {
+
+document.addEventListener('click', function () {
   const customMenu = document.getElementById('custom-context-menu')
   if (customMenu.style.display === 'block') {
     customMenu.style.display = 'none'
   }
 })
 
+
 const observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     mutation.addedNodes.forEach((node) => {
       if (node.matches('li')) {
         filterStreamers() // 새로운 <li>가 추가될 때마다 필터링 적용
-        node.addEventListener('contextmenu', handleCustomMenu)
+
+        // /lives 외에서는 제대로 작동하지않아 전체 li
+        document.querySelectorAll('li').forEach((queryNode) => {
+          queryNode.addEventListener('contextmenu', handleCustomMenu)
+        })
         removeSet.forEach(node => node.remove())
         removes = []
         removeSet = new Set()
